93a3f5f62869f43b08120a3b8a78071b46b8d05b
Umar Khalilov <ERMASTER100@gmail.com>
2021-10-26 17:53:15 +0300

    Add UserList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import { Admin } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import './App.css';
+import { UserList } from './components/UserList';
+
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-export const App = () => <Admin dataProvider={dataProvider} />;
+export const App = () => (
+    <Admin dataProvider={dataProvider}>
+        <Resource name='users' list={UserList} />
+    </Admin>
+);
