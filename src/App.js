4f7502d025d8ef1350256a260c4bc118fa2787a0
Umar Khalilov <ERMASTER100@gmail.com>
2021-10-26 23:42:19 +0300

    Refactor code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './components/UserList';
 
-
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 export const App = () => (
     <Admin dataProvider={dataProvider}>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
-import './index.css';
 import { App } from './App';
-
+import './index.css';
 
 render(
     <StrictMode>
         <App />
     </StrictMode>,
-    document.getElementById('root'),
+    document.getElementById('root')
 );
